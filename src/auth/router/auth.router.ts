613bf18535607a8168bfa25c947007a081c34212
markdave123-py <nwaekwudavid@gmail.com>
2023-09-27 02:04:26 +0100

    Refactoring

---
+++ src/auth/router/auth.router.ts
@@ -0,0 +1,55 @@
+// import { Router } from "express"
+// import { loginSchema, signUpSchema } from "./schema"
+// import {
+//   login,
+//   signup,
+//   logout,
+//   roleService,
+//   refreshAccessToken,
+//   getAuthUser,
+// } from "../services"
+// import { ControlBuilder } from "../../core/middlewares/controlBuilder"
+
+// export const authRouter = Router()
+
+// authRouter
+//   .get(
+//     "/user",
+//     ControlBuilder.builder()
+//       .setHandler(getAuthUser.handle)
+//       .isPrivate()
+//       .handle(),
+//   )
+//   .post(
+//     "/login",
+//     ControlBuilder.builder()
+//       .setHandler(login.handle)
+//       .setValidator(loginSchema)
+//       .handle(),
+//   )
+
+//   .post(
+//     "/signup",
+//     ControlBuilder.builder()
+//       .setHandler(signup.handle)
+//       .setValidator(signUpSchema)
+//       .handle(),
+//   )
+//   .post(
+//     "/logout",
+//     ControlBuilder.builder()
+//       .setHandler(logout.handle)
+//       .isPrivate()
+
+//       .handle(),
+//   )
+
+//   .get(
+//     "/roles",
+//     ControlBuilder.builder().setHandler(roleService.findAll).handle(),
+//   )
+
+//   .get(
+//     "/refresh-token",
+//     ControlBuilder.builder().setHandler(refreshAccessToken.refresh).handle(),
+//   )
